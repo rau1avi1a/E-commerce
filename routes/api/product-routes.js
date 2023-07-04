c8415ce39d1358ff9b053910a9318f3470e5d0fa
rau1avi1a <avila0118@icloud.com>
2023-07-04 14:18:09 -0700

    updated api route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -57,10 +57,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
@@ -130,7 +130,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const deleteProduct = await Tag.destroy(
+    const deleteProduct = await Product.destroy(
       {
         where: { 
           id: req.params.id,
